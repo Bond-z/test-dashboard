e4ecccfba2b989548def454130141219b47fd8aa
Bond-z <nathakrit.p@gmail.com>
2024-04-09 14:11:32 +0700

    edit setting to be ready to deploy

--- myframework/myframework/settings.py
+++ myframework/myframework/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cr(^u!*=xu5e!(x4*e_w*r0i&^-zcr75v!#enzw0dddgl*!206'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
